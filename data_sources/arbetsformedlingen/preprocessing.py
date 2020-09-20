59296a6e705fada8e856737480f873f033aaf6b1
Emil Wåreus <emil.wareus@debricked.com>
2020-09-20 12:23:59 +0200

    fixed encoding errors and initial modeling

--- data_sources/arbetsformedlingen/preprocessing.py
+++ data_sources/arbetsformedlingen/preprocessing.py
@@ -6,7 +6,7 @@ import numpy as np
 
 _columns_to_drop = ['access', 'access_to_own_car', 'driving_license', 'driving_license_required', 'experience_required', 'logo_url', 'removed', 'removed_date', 'source_type', 'timestamp', 'webpage_url', 'application_details_email', 'application_details_information', 'application_details_other', 'application_details_reference', 'application_details_via_af', 'description_needs', 'description_requirements', 'description_text_formatted', 'duration_concept_id', 'duration_legacy_ams_taxonomy_id', 'employer_email', 'employer_organization_number', 'employer_phone_number', 'employer_url', 'employment_type_concept_id', 'employment_type_legacy_ams_taxonomy_id', 'must_have_languages', 'must_have_skills', 'must_have_work_experiences', 'nice_to_have_languages', 'nice_to_have_skills',
                     'nice_to_have_work_experiences', 'occupation_concept_id', 'occupation_label', 'occupation_field_concept_id', 'occupation_field_label', 'occupation_field_legacy_ams_taxonomy_id', 'occupation_group_concept_id', 'occupation_group_label', 'occupation_group_legacy_ams_taxonomy_id', 'salary_type_concept_id', 'salary_type_legacy_ams_taxonomy_id', 'scope_of_work_max', 'scope_of_work_min', 'working_hours_type_concept_id', 'working_hours_type_legacy_ams_taxonomy_id', 'workplace_address_coordinates', 'workplace_address_country_code', 'workplace_address_country_concept_id', 'workplace_address_municipality', 'workplace_address_municipality_code', 'workplace_address_municipality_concept_id', 'workplace_address_region', 'workplace_address_region_code', 'workplace_address_region_concept_id']
-                    
+
 
 def open_file(filename):
     with open(filename) as f:
@@ -66,7 +66,8 @@ def preprocess_file(filename):
 def append_iter_csv(df, name):
     chunks = int(df.shape[0] / 1000)
     for chunk in tqdm(np.array_split(df, chunks)):
-        chunk.to_csv(name, mode='a', header=False)
+        chunk.to_csv(name, mode='a', header=False,
+                     encoding='ISO-8859-1', errors='replace')
 
 
 def preprocess_all_files():
@@ -78,7 +79,7 @@ def preprocess_all_files():
         df = preprocess_file("./downloads/" + file)
 
         if first:
-            df.head(0).to_csv('af_prepro.csv')
+            df.head(0).to_csv('af_prepro.csv', encoding='ISO-8859-1')
             first = False
 
         append_iter_csv(df, 'af_prepro.csv')
